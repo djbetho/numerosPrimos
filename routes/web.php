94e25c371b041c1b2963b986e98035d513c8b0f7
Ruperto Bugueño <ru.bugueno@gmail.com>
2022-12-23 03:09:03 -0300

    creacion de la vista y la ruta

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('primos');
 });
+
+  Route::get('/primos','App\Http\Controllers\PrimosController@index')->name('primos.index');
+  
